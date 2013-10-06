be98c2bf3f86d2b1070cb2522992d08893d8cd55
toandv55 <toandv_55@vnu.edu.vn>
2013-10-06 11:51:24 +0700

    first uploading

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Student::Application.config.secret_token = '2d0848c1245b23505a3e76ee65a9c837c24d72505e3b643f5ba87982d71fbc7bdc02e60517f87ef6503594f10d13226d037aba571e65a644bfe51969ae4d0953'
